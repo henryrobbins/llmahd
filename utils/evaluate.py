c1b1e80d66c3ea66fe073f75dbf43a3d53344c03
Henry Robbins <hw.robbins@gmail.com>
2025-11-29 12:58:22 -0800

    Use Problem class as base for Evaluator class

---
+++ utils/evaluate.py
@@ -0,0 +1,187 @@
+import logging
+import subprocess
+from utils.problem import ProblemPrompts, adapt_prompt
+from utils.utils import block_until_running, filter_traceback
+
+
+class Evaluator:
+    def __init__(self, problem_name: str, root_dir: str):
+        self.root_dir = root_dir
+
+        self.problem_config = ProblemPrompts.load_problem_prompts(
+            f"{self.root_dir}/prompts/{problem_name}"
+        )
+
+        self.output_file = (
+            f"{self.root_dir}/problems/{self.problem_config.problem_name}/gpt.py"
+        )
+
+        if self.problem_config.problem_type == "constructive":
+            from utils.problem import TSP_CONSTRUCTIVE_PROMPTS
+
+            self.prompts = TSP_CONSTRUCTIVE_PROMPTS
+        elif self.problem_config.problem_type == "online":
+            from utils.problem import BPP_ONLINE_PROMPTS
+
+            self.prompts = BPP_ONLINE_PROMPTS
+        else:
+            self.prompts = adapt_prompt(self.problem_config)
+
+        self.function_evals = 0
+
+    def response_to_individual(self, code, response_id, file_name=None) -> dict:
+        """
+        Convert response to individual
+        """
+        # Write response to file
+        file_name = (
+            f"problem_iter{self.iteration}_response{response_id}.txt"
+            if file_name is None
+            else file_name + ".txt"
+        )
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.writelines(code + "\n")
+
+        # Extract code and description from response
+        std_out_filepath = (
+            f"problem_iter{self.iteration}_stdout{response_id}.txt"
+            if file_name is None
+            else file_name.rstrip(".txt") + "_stdout.txt"
+        )
+
+        individual = {
+            "stdout_filepath": std_out_filepath,
+            "code_path": f"problem_iter{self.iteration}_code{response_id}.py",
+            "code": code,
+            "response_id": response_id,
+        }
+        return individual
+
+    def mark_invalid_individual(self, individual: dict, traceback_msg: str) -> dict:
+        """
+        Mark an individual as invalid.
+        """
+        individual["exec_success"] = False
+        individual["obj"] = float("inf")
+        individual["traceback_msg"] = traceback_msg
+        return individual
+
+    def batch_evaluate(self, codes: list[str], iteration: int) -> str | list[float]:
+        """
+        Evaluate population by running code in parallel and computing objective values.
+        """
+        self.iteration = iteration
+
+        population = [
+            self.response_to_individual(resp, index) for index, resp in enumerate(codes)
+        ]
+
+        inner_runs = []
+        # Run code to evaluate
+        for response_id in range(len(population)):
+            self.function_evals += 1
+            # Skip if response is invalid
+            if population[response_id]["code"] is None:
+                population[response_id] = self.mark_invalid_individual(
+                    population[response_id], "Invalid response!"
+                )
+                inner_runs.append(None)
+                continue
+
+            logging.info(f"Iteration {self.iteration}: Running Code {response_id}")
+
+            try:
+                process = self._run_code(population[response_id], response_id)
+                inner_runs.append(process)
+            except Exception as e:  # If code execution fails
+                logging.info(f"Error for response_id {response_id}: {e}")
+                population[response_id] = self.mark_invalid_individual(
+                    population[response_id], str(e)
+                )
+                inner_runs.append(None)
+
+        # Update population with objective values
+        for response_id, inner_run in enumerate(inner_runs):
+            if inner_run is None:  # If code execution fails, skip
+                continue
+            try:
+                inner_run.communicate(
+                    timeout=self.config.timeout
+                )  # Wait for code execution to finish
+            except subprocess.TimeoutExpired as e:
+                logging.info(f"Error for response_id {response_id}: {e}")
+                population[response_id] = self.mark_invalid_individual(
+                    population[response_id], str(e)
+                )
+                inner_run.kill()
+                continue
+
+            individual = population[response_id]
+            stdout_filepath = individual["stdout_filepath"]
+            with open(stdout_filepath, "r") as f:  # read the stdout file
+                stdout_str = f.read()
+            traceback_msg = filter_traceback(stdout_str)
+
+            # Store objective value for each individual
+            if traceback_msg == "":  # If execution has no error
+                try:
+                    individual["obj"] = float(stdout_str.split("\n")[-2])
+                    assert (
+                        individual["obj"] > 0
+                    ), "Objective value <= 0 is not supported."
+                    individual["obj"] = (
+                        -individual["obj"]
+                        if self.problem_config.obj_type == "max"
+                        else individual["obj"]
+                    )
+                    individual["exec_success"] = True
+                except:
+                    population[response_id] = self.mark_invalid_individual(
+                        population[response_id], "Invalid std out / objective value!"
+                    )
+            else:  # Otherwise, also provide execution traceback error feedback
+                population[response_id] = self.mark_invalid_individual(
+                    population[response_id], traceback_msg
+                )
+
+            logging.info(
+                f"Iteration {self.iteration}, response_id {response_id}: Objective value: {individual['obj']}"
+            )
+        return population
+
+    def _run_code(self, individual: dict, response_id) -> subprocess.Popen:
+        """
+        Write code into a file and run eval script.
+        """
+        logging.debug(f"Iteration {self.iteration}: Processing Code Run {response_id}")
+
+        with open(self.output_file, "w") as file:
+            file.writelines(individual["code"] + "\n")
+
+        # Execute the python file with flags
+        with open(individual["stdout_filepath"], "w") as f:
+            eval_file_path = (
+                f"{self.root_dir}/problems/{self.prompts.problem_name}/eval.py"
+                if self.prompts.problem_type != "black_box"
+                else f"{self.root_dir}/problems/{self.prompts.problem_name}/eval_black_box.py"
+            )
+            process = subprocess.Popen(
+                [
+                    "python",
+                    "-u",
+                    eval_file_path,
+                    f"{self.prompts.problem_size}",
+                    self.root_dir,
+                    "train",
+                ],
+                stdout=f,
+                stderr=f,
+            )
+
+        block_until_running(
+            individual["stdout_filepath"],
+            log_status=True,
+            iter_num=self.iteration,
+            response_id=response_id,
+        )
+        return process
